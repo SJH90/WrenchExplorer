f464219b4b94a33af356530a184f8fa5e0ed373f
SSK <hot02toy@naver.com>
2017-08-01 17:47:26 +0900

    pushpush - 2017-08-01 17:47:26.10 by hot02

---
+++ Assets/SSK/Punch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour {
+    Rigidbody rigid;
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void punch()
+    {
+        print("punch!");
+        rigid.AddForce(100 * Vector3.forward);
+    }
+}
--- Assets/SSK/gongguligi.cs
+++ Assets/SSK/gongguligi.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class gongguligi : MonoBehaviour {
     SpringJoint spring;
+    Punch punch;
 	// Use this for initialization
 	void Start () {
         rigid = GetComponent<Rigidbody>();
         spring=GetComponentInChildren<SpringJoint>();
+        punch = GetComponentInChildren<Punch>();
     }
     Rigidbody rigid;
 	// Update is called once per frame
 	void Update () {
         float h=Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        rigid.AddTorque(Vector3.back * h*2);
-        rigid.AddTorque(Vector3.right * v*2);
+        rigid.AddTorque(Vector3.back * h*200);
+        rigid.AddTorque(Vector3.right * v*200);
+        if (Input.GetKey(KeyCode.Z))
+        {
+            punch.punch();
+        }
         Vector3 springForce= spring.currentForce;
     }
 }
