4d13731ef32bd4efd559942f23d6e75e3bb24414
sjh90 <wlghks1603@naver.com>
2017-08-03 16:23:08 +0900

    JHS

---
+++ Assets/JHS/Scripts/VREyeRaycast.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class VREyeRaycast : MonoBehaviour {
+
+    
+
+    [SerializeField]    private VRInput m_VRInput;
+    [SerializeField]    private Transform m_Camera;
+    [SerializeField]    private Transform m_Reticle;
+
+    private VRInteractiveItem m_CurrentInteractiveItem;
+    
+
+
+
+
+    // Use this for initialization
+    void Awake   ()
+    {
+        m_VRInput.OnSwipe += HandleSwipe;
+        m_VRInput.OnClick += HandleClick;
+        m_VRInput.OnCancel += HandleCancel;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        EyeRaycast();
+	}
+    
+    private void EyeRaycast()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(m_Camera.position, m_Camera.forward);
+
+        VRInteractiveItem interactible;
+
+        if (Physics.Raycast(ray, out hit, 500f))
+        {
+            m_Reticle.position = Vector3.Lerp(Camera.main.transform.position, hit.point, 0.9f);
+            m_Reticle.localScale = new Vector3(0.01f, 0.01f, 0.01f) * Vector3.Distance(Camera.main.transform.position, m_Reticle.position);
+            
+            interactible = hit.collider.GetComponent<VRInteractiveItem>(); //attempt to get the VRInteractiveItem on the hit object
+        }
+        else
+        {
+            interactible = null;
+        }
+
+        if (m_CurrentInteractiveItem != interactible)
+        {
+
+            if (m_CurrentInteractiveItem != null)
+                m_CurrentInteractiveItem.Exit();
+
+            m_CurrentInteractiveItem = interactible;
+
+            if (m_CurrentInteractiveItem != null)
+                m_CurrentInteractiveItem.Enter();
+                
+        }
+        else
+        {
+            if (m_CurrentInteractiveItem != null)
+                m_CurrentInteractiveItem.Over();
+        }
+    }
+
+    void HandleClick()
+    {
+
+        if (m_CurrentInteractiveItem != null)
+        {
+            m_CurrentInteractiveItem.Click();
+        }
+
+
+    }
+    void HandleCancel()
+    {
+        if (m_CurrentInteractiveItem != null)
+            m_CurrentInteractiveItem.Cancel();
+    }
+
+    void HandleSwipe(VRInput.SwipeDirection swipe)
+    {
+        //GameManager.Instance.InputSwipe(swipe);
+    }
+    
+}
---
+++ Assets/JHS/Scripts/VRInteractiveItem.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class VRInteractiveItem : MonoBehaviour {
+    
+    public event Action OnEnter;
+    public event Action OnOver;
+    public event Action OnExit;
+
+    public event Action OnClick;
+    public event Action OnCancel;
+    
+    public void Enter()
+    {
+        if (OnEnter != null)
+            OnEnter();
+    }
+    public void Over()
+    {
+        if (OnOver != null)
+            OnOver();
+    }
+    public void Exit()
+    {
+        if (OnExit != null)
+            OnExit();
+    }
+    public void Click()
+    {
+        if (OnClick != null)
+            OnClick();
+    }
+    public void Cancel()
+    {
+        if (OnCancel != null)
+            OnCancel();
+    }
+
+    private void OnDestroy()
+    {
+        OnEnter = null;
+        OnOver = null;
+        OnExit = null;
+
+        OnClick = null;
+        OnCancel = null;
+    }
+}
