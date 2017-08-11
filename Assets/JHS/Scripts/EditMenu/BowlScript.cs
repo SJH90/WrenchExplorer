d90111ad6719f1753972cafc67867c36edce4541
unknown <quruquru91@hanmail.net>
2017-08-11 17:27:58 +0900

    fefef

---
+++ Assets/JHS/Scripts/EditMenu/BowlScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipInsideOut : MonoBehaviour
+{
+    public int[] Tris = new int[100];
+    int temp;
+    
+    void Start()
+    {
+        Tris = this.GetComponent<MeshFilter>().mesh.triangles;
+        for (int i = 0; i < 100; i++)
+        {
+            if (Tris[3 * i] != null)
+            {
+                temp = Tris[3 * i + 1];
+                Tris[3 * i + 1] = Tris[3 * i + 2];
+                Tris[3 * i + 2] = temp;
+            }
+        }
+        this.GetComponent<MeshFilter>().mesh.triangles = Tris;
+    }
+}
